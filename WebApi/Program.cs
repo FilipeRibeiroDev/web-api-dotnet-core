57dd27835f2bd84d64b082a7bc47bd719a2caf4e
Filipe Ribeiro <68390743+FilipeRibeiroDev@users.noreply.github.com>
2023-02-08 16:02:46 -0300

    Adicionar CORS

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -74,6 +74,16 @@ builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerGenOptions>();
 
 
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: "MyPolicy",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:8080")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
 var key = Encoding.ASCII.GetBytes(WebApi.Key.Secret);
 
 builder.Services.AddAuthentication(x =>
@@ -114,6 +124,9 @@ else
 {
     app.UseExceptionHandler("/error");
 }
+
+
+app.UseCors("MyPolicy");
 
 app.UseHttpsRedirection();
 
